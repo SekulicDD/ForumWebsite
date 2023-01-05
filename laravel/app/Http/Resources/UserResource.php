d275bcff3f530c7fb0c86bfcb3d1ef8c27dd591d
SekulicDD <sekulicdd@gmail.com>
2023-01-05 20:23:56 +0100

    Add resources

---
+++ laravel/app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'image'=>ImageResource::make($this->whenLoaded('image')),
+            'role'=>RoleResource::make($this->whenLoaded('role')),
+            'friends'=>UserResource::collection($this->whenLoaded('friends'))
+        ];
+    }
+}
